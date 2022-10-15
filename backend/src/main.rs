cc875b4da148e0eeb82e7dd068a376b82fb63085
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2022-10-15 02:35:55 -0300

    feat: http server working

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+const IPV4: &str = "0.0.0.0";
+const PORT: u16 = 7780;
+const HOST: (&str, u16) = (IPV4, PORT);
+
+#[get("/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("🚀 Serving on {IPV4}:{PORT}");
+
+    HttpServer::new(|| App::new().service(web::scope("/api").service(echo).service(hello)))
+        .bind(HOST)?
+        .run()
+        .await
 }
