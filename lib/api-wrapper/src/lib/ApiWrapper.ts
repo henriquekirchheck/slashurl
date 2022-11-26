428c11444bc9d61f1b787ca06dac7735ab281d67
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2022-11-26 12:56:20 -0300

    feat: added url endpoints

--- lib/api-wrapper/src/lib/ApiWrapper.ts
+++ lib/api-wrapper/src/lib/ApiWrapper.ts
@@ -1,6 +1,12 @@
 import axios, { AxiosRequestConfig } from "axios"
 
 export type ResultType<T> = { error?: Error | string; data?: T }
+export type UrlModelType = {
+  short_url: string
+  full_url: string
+  created_at: Date
+  views: number
+}
 
 export class SlashUrlApiWrapper {
   #baseUrl: string
@@ -8,7 +14,23 @@ export class SlashUrlApiWrapper {
     hello: (): AxiosRequestConfig => ({
       baseURL: this.#baseUrl,
       url: "/hello",
-      method: "GET",
+      method: "get",
+      headers: {
+        Accept: "application/json",
+      },
+    }),
+    list_urls: (): AxiosRequestConfig => ({
+      baseURL: this.#baseUrl,
+      url: "/api/url",
+      method: "get",
+      headers: {
+        Accept: "application/json",
+      },
+    }),
+    get_url: (pathParam: string): AxiosRequestConfig => ({
+      baseURL: this.#baseUrl,
+      url: `/api/url/${pathParam}`,
+      method: "get",
       headers: {
         Accept: "application/json",
       },
@@ -30,7 +52,29 @@ export class SlashUrlApiWrapper {
   async helloWorld(): Promise<ResultType<string>> {
     const config = this.#endpoints.hello()
     try {
-      const data = await axios.request<string>({ ...config }).then((res) => res.data)
+      const data = await axios
+        .request<string>({ ...config })
+        .then((res) => res.data)
+      return { data }
+    } catch (error) {
+      return this.#getError(error)
+    }
+  }
+
+  async urlInfo(id?: string): Promise<ResultType<UrlModelType | UrlModelType[]>> {
+    try {
+      if (id) {
+        const config = this.#endpoints.get_url(id)
+        const data = await axios
+          .request<UrlModelType>({ ...config })
+          .then((res) => res.data)
+        return { data }
+      }
+
+      const config = this.#endpoints.list_urls()
+      const data = await axios
+        .request<UrlModelType[]>({ ...config })
+        .then((res) => res.data)
       return { data }
     } catch (error) {
       return this.#getError(error)
