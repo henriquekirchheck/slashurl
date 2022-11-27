2464ed9a4b81f2f00d2152c99f2c4eec5b76ecc8
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2022-11-27 12:13:13 -0300

    feat: started bot dev

--- app/discord-bot/src/main.ts
+++ app/discord-bot/src/main.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+import { Client, Events, GatewayIntentBits } from "discord.js"
+
+const token = process.env.DISCORD_BOT_TOKEN
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`)
+})
+
+client.login(token)
