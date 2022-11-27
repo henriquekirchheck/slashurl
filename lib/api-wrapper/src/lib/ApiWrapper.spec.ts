c80595256a744a35866e205c39918ecfa3fc639a
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2022-11-26 22:19:29 -0300

    fix: added get url tests and fixed errors with recent changes

--- lib/api-wrapper/src/lib/ApiWrapper.spec.ts
+++ lib/api-wrapper/src/lib/ApiWrapper.spec.ts
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { ResultType, SlashUrlApiWrapper } from "./ApiWrapper"
+import { ResponseType, SlashUrlApiWrapper, UrlModelType } from "./ApiWrapper"
 
 jest.mock("axios")
 const mockAxios = axios as jest.Mocked<typeof axios>
@@ -8,14 +8,13 @@ describe("apiWrapper", () => {
   const baseUrl = "http://127.0.0.1:8080/"
   const wrapper = new SlashUrlApiWrapper(baseUrl)
 
-  it("should work", () => {
-    expect("hi").toEqual("hi")
-  })
-
   beforeEach(() => {
     jest.clearAllMocks()
   })
 
+  it("should work", () => {
+    expect("hi").toEqual("hi")
+  })
   it("should get hello world", async () => {
     const thenFn = jest.fn()
     const catchFn = jest.fn()
@@ -28,13 +27,13 @@ describe("apiWrapper", () => {
 
     await wrapper.helloWorld().then(thenFn).catch(catchFn)
 
-    expect(thenFn).toHaveBeenCalledWith<[ResultType<string>]>({
+    expect(thenFn).toHaveBeenCalledWith<[ResponseType<string>]>({
+      success: true,
       data: serverResponse,
     })
 
     expect(catchFn).not.toHaveBeenCalled()
   })
-
   it("should return error case", async () => {
     const thenFn = jest.fn()
     const catchFn = jest.fn()
@@ -45,8 +44,65 @@ describe("apiWrapper", () => {
 
     await wrapper.helloWorld().then(thenFn).catch(catchFn)
 
-    expect(thenFn).toHaveBeenCalledWith<[ResultType<string>]>({
-      error: axiosError.message,
+    expect(thenFn).toHaveBeenCalledWith<[ResponseType<string>]>({
+      success: false,
+      error: axiosError,
+    })
+
+    expect(catchFn).not.toHaveBeenCalled()
+  })
+  it("should get all ids", async () => {
+    const thenFn = jest.fn()
+    const catchFn = jest.fn()
+
+    const serverResponse: UrlModelType[] = [
+      {
+        short_url: "abcdefgh",
+        full_url: "https://kernel.org/",
+        created_at: new Date(),
+        views: 70 - 1,
+      },
+      {
+        short_url: "goooogle",
+        full_url: "https://google.com/",
+        created_at: new Date(),
+        views: 70 - 1,
+      },
+    ]
+
+    mockAxios.request.mockResolvedValueOnce({
+      data: serverResponse,
+    })
+
+    await wrapper.urlInfo().then(thenFn).catch(catchFn)
+
+    expect(thenFn).toHaveBeenCalledWith<[ResponseType<UrlModelType[]>]>({
+      success: true,
+      data: serverResponse,
+    })
+
+    expect(catchFn).not.toHaveBeenCalled()
+  })
+  it("should get one id", async () => {
+    const thenFn = jest.fn()
+    const catchFn = jest.fn()
+
+    const serverResponse: UrlModelType = {
+      short_url: "abcdefgh",
+      full_url: "https://kernel.org/",
+      created_at: new Date(),
+      views: 70 - 1,
+    }
+
+    mockAxios.request.mockResolvedValueOnce({
+      data: serverResponse,
+    })
+
+    await wrapper.urlInfo("abcdefgh").then(thenFn).catch(catchFn)
+
+    expect(thenFn).toHaveBeenCalledWith<[ResponseType<UrlModelType>]>({
+      success: true,
+      data: serverResponse,
     })
 
     expect(catchFn).not.toHaveBeenCalled()
