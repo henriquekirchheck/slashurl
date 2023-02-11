aaecf903fb74de8faa66eece0f0ca030be2d2cb5
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-02-10 21:05:50 -0300

    feat: created website Vite boilerplate

---
+++ app/website/vite.config.ts
@@ -0,0 +1,44 @@
+import { svelte } from "@sveltejs/vite-plugin-svelte"
+/// <reference types="vitest" />
+import { defineConfig } from "vite"
+
+import viteTsConfigPaths from "vite-tsconfig-paths"
+
+export default defineConfig({
+  cacheDir: "../../node_modules/.vite/website",
+
+  server: {
+    port: 4200,
+    host: "localhost",
+  },
+
+  preview: {
+    port: 4300,
+    host: "localhost",
+  },
+
+  plugins: [
+    viteTsConfigPaths({
+      root: "../../",
+    }),
+    svelte(),
+  ],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [
+  //    viteTsConfigPaths({
+  //      root: '../../',
+  //    }),
+  //  ],
+  // },
+
+  test: {
+    globals: true,
+    cache: {
+      dir: "../../node_modules/.vitest",
+    },
+    environment: "jsdom",
+    include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+  },
+})
