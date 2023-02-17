0f752f32795654b8031d3f758186120b161f23c2
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-02-17 16:57:15 -0300

    BREAKING CHANGE: changed fetch functions to higher order functions for compatibility in browsers

--- lib/api-wrapper/src/lib/ApiWrapper.ts
+++ lib/api-wrapper/src/lib/ApiWrapper.ts
@@ -1,4 +1,4 @@
-import { z, ZodType } from "zod"
+import { z, ZodType, ZodTypeAny } from "zod"
 import { fetchData } from "./fetch"
 import { Err, Ok, Result } from "./result"
 
@@ -29,62 +29,69 @@ function changeDateISOStringToDate(data: UrlModelType): UrlModelDateType {
   }
 }
 
-type FetchConfigType = readonly [input: URL, init: RequestInit]
+type ApiConstructorType<V extends ZodType> = {
+  config: readonly [input: URL, init: RequestInit]
+  validator: V
+}
 
 const fetchApiConstructor = {
-  hello: (apiBaseUrl: URL | string) => ({
-    config: [
-      new URL("/hello", apiBaseUrl),
-      {
-        method: "GET",
-        headers: {
-          Accept: "application/json",
+  hello: (apiBaseUrl: URL | string) =>
+    ({
+      config: [
+        new URL("/hello", apiBaseUrl),
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+          },
         },
-      },
-    ] satisfies FetchConfigType,
-    validator: z.string(),
-  }),
+      ],
+      validator: z.string(),
+    } satisfies ApiConstructorType<ZodTypeAny>),
 
-  list_urls: (apiBaseUrl: URL | string) => ({
-    config: [
-      new URL("/api/url", apiBaseUrl),
-      {
-        method: "get",
-        headers: {
-          Accept: "application/json",
+  list_urls: (apiBaseUrl: URL | string) =>
+    ({
+      config: [
+        new URL("/api/url", apiBaseUrl),
+        {
+          method: "get",
+          headers: {
+            Accept: "application/json",
+          },
         },
-      },
-    ] satisfies FetchConfigType,
-    validator: z.array(UrlModel),
-  }),
+      ],
+      validator: z.array(UrlModel),
+    } satisfies ApiConstructorType<ZodTypeAny>),
 
-  get_url: (apiBaseUrl: URL | string, pathParam: string) => ({
-    config: [
-      new URL(`/api/url/${pathParam}`, apiBaseUrl),
-      {
-        method: "get",
-        headers: {
-          Accept: "application/json",
+  get_url: (apiBaseUrl: URL | string, pathParam: string) =>
+    ({
+      config: [
+        new URL(`/api/url/${pathParam}`, apiBaseUrl),
+        {
+          method: "get",
+          headers: {
+            Accept: "application/json",
+          },
         },
-      },
-    ] satisfies FetchConfigType,
-    validator: UrlModel,
-  }),
+      ],
+      validator: UrlModel,
+    } satisfies ApiConstructorType<ZodTypeAny>),
 
-  create_url: (apiBaseUrl: URL | string, full_url: string) => ({
-    config: [
-      new URL("/api/url", apiBaseUrl),
-      {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
+  create_url: (apiBaseUrl: URL | string, full_url: string) =>
+    ({
+      config: [
+        new URL("/api/url", apiBaseUrl),
+        {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify({ full_url }),
         },
-        body: JSON.stringify({ full_url }),
-      },
-    ] satisfies FetchConfigType,
-    validator: UrlModel,
-  }),
+      ],
+      validator: UrlModel,
+    } satisfies ApiConstructorType<ZodTypeAny>),
 } as const
 
 async function getReturn<T>(fetch: Promise<T>): Promise<Result<T, Error>> {
@@ -96,91 +103,68 @@ async function getReturn<T>(fetch: Promise<T>): Promise<Result<T, Error>> {
   }
 }
 
-async function parseJsonWithValidador<V extends ZodType>({
-  fetch,
+const parseFinalValue = async <V extends ZodType>({
+  config,
   validator,
-}: {
-  fetch: Promise<Response>
-  validator: V
-}): Promise<z.infer<V>> {
-  return fetch.then((res) => res.json()).then(validator.parse)
+}: ApiConstructorType<V>): Promise<z.infer<V>> => {
+  return await fetchData(...config)
+    .then((res) => res.json())
+    .then(validator.parse)
 }
 
 export class SlashUrlApiWrapper {
-  #baseUrl: string
+  baseUrl: string
 
   constructor(baseUrl: string) {
-    this.#baseUrl = baseUrl
+    this.baseUrl = baseUrl
   }
 
-  public get baseUrl(): string {
-    return this.#baseUrl
-  }
-
-  #helloModel() {
-    const hello = fetchApiConstructor.hello(this.#baseUrl)
-    return { fetch: fetchData(...hello.config), validator: hello.validator }
-  }
-
-  public helloFetch() {
-    return parseJsonWithValidador(this.#helloModel())
+  public getHelloFetch() {
+    return () => parseFinalValue(fetchApiConstructor.hello(this.baseUrl))
   }
 
   public helloWorld() {
-    return getReturn(this.helloFetch())
-  }
-
-  #getUrlInfoModel(id: string) {
-    const urlInfo = fetchApiConstructor.get_url(this.#baseUrl, id)
-    return { fetch: fetchData(...urlInfo.config), validator: urlInfo.validator }
+    return getReturn(this.getHelloFetch()())
   }
 
   public getUrlInfoFetch(id: string) {
-    return parseJsonWithValidador(this.#getUrlInfoModel(id)).then(
-      changeDateISOStringToDate
-    )
+    return () =>
+      parseFinalValue(fetchApiConstructor.get_url(this.baseUrl, id)).then(
+        changeDateISOStringToDate
+      )
   }
 
-  #listUrlsInfoModel() {
-    const urlsInfo = fetchApiConstructor.list_urls(this.#baseUrl)
-    return {
-      fetch: fetchData(...urlsInfo.config),
-      validator: urlsInfo.validator,
-    }
-  }
-
-  public listUrlsInfoFetch() {
-    return parseJsonWithValidador(this.#listUrlsInfoModel()).then((data) =>
-      data.map(changeDateISOStringToDate)
-    )
+  public getListUrlsInfoFetch() {
+    return () =>
+      parseFinalValue(fetchApiConstructor.list_urls(this.baseUrl)).then(
+        (data) => data.map(changeDateISOStringToDate)
+      )
   }
 
   public urlInfo(
     id?: string
   ): Promise<Result<UrlModelDateType | UrlModelDateType[], Error>> {
     if (id) {
-      return getReturn(this.getUrlInfoFetch(id))
+      return getReturn(this.getUrlInfoFetch(id)())
     }
-    return getReturn(this.listUrlsInfoFetch())
+    return getReturn(this.getListUrlsInfoFetch()())
   }
 
-  #createUrlModel(full_url: string) {
-    const urlInfo = fetchApiConstructor.create_url(this.#baseUrl, full_url)
-    return { fetch: fetchData(...urlInfo.config), validator: urlInfo.validator }
-  }
-
-  public createUrlFetch(full_url: string): Promise<CreateUrlResponseType> {
-    return parseJsonWithValidador(this.#createUrlModel(full_url))
-      .then(changeDateISOStringToDate)
-      .then((data) => ({
-        info: data,
-        url: new URL(`/${data.short_url}`, this.#baseUrl).toString(),
-      }))
+  public getCreateUrlFetch(
+    full_url: string
+  ): () => Promise<CreateUrlResponseType> {
+    return () =>
+      parseFinalValue(fetchApiConstructor.create_url(this.baseUrl, full_url))
+        .then(changeDateISOStringToDate)
+        .then((data) => ({
+          info: data,
+          url: new URL(`/${data.short_url}`, this.baseUrl).toString(),
+        }))
   }
 
   public createUrl(
     fullUrl: URL | string
   ): Promise<Result<CreateUrlResponseType, Error>> {
-    return getReturn(this.createUrlFetch(fullUrl.toString()))
+    return getReturn(this.getCreateUrlFetch(fullUrl.toString())())
   }
 }
